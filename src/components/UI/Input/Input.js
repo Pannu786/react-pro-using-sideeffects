b8f63be483f81a551a2cc2e541af36bdab3a59c7
Pannu786 <thepannu786@gmail.com>
2022-01-11 10:01:03 +0530

    created Input.js making Login input reusable

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,22 @@
+import style from './Input.module.css';
+
+const Input = (props) => {
+  return (
+    <div
+      className={`${style.control} ${
+        props.isValid === false ? style.invalid : ''
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+};
+
+export default Input;
